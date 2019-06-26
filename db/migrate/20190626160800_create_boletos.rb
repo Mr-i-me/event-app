f2cc446531f0cff253fc76f7f08ff5b407b86d77
gabriel schiavo <gabriel@lewagon.org>
2019-06-26 14:24:11 -0300

    add boleto migration

--- db/migrate/20190626160800_create_boletos.rb
+++ db/migrate/20190626160800_create_boletos.rb
@@ -1,9 +1,9 @@
 class CreateBoletos < ActiveRecord::Migration[5.2]
   def change
     create_table :boletos do |t|
-      t.integer :sku
-      t.integer :value 
-      t.date :expiration_date
+      t.integer :sku, null: false
+      t.integer :value, null: false 
+      t.date :expiration_date, null: false
       t.integer :interest
       t.references :user
 
